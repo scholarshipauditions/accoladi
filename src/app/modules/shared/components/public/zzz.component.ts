fd501f975f75112ca4a5909c4dc0f1cbadb7baa7
jpmyob <james@scholarshipauditions.com>
2021-08-19 12:23:33 -0500

    Build 20210819 - typo fixes

---
+++ src/app/modules/shared/components/public/zzz.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+	selector: 'app-zzz',
+	templateUrl: './zzz.component.html',
+	styleUrls: ['./public.component.css']
+})
+
+export class ZzzComponent {
+
+	constructor() { }
+
+}
